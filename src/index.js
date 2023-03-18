ffea0c6d46c16e0423511496633ec9b7e97f6544
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-03-18 22:45:26 +0300

    dark mode

--- src/index.js
+++ src/index.js
@@ -43,13 +43,13 @@ const navigationTwo = [
 ];
 
 const navDiv = document.createElement("div"); // div elementi oluşturuldu.
-navDiv.classList = 'mb-10'
+navDiv.classList = "mb-10";
 navigation.forEach((item) => {
   const navA = document.createElement("a");
   navA.textContent = item.name;
   navA.href = "#";
   navA.classList =
-    "flex items-center h-10 rounded px-3 text-sm text-logo font-medium hover:bg-gray-100 transition-colors";
+    "flex items-center h-10 rounded px-3 text-xl text-logo font-bold hover:bg-gray-100 transition-colors dark:text-white dark:hover:!bg-gray-800";
   navDiv.appendChild(navA);
 });
 
@@ -59,7 +59,7 @@ navigationTwo.forEach((item) => {
   navA.textContent = item.name;
   navA.href = "#";
   navA.classList =
-    "flex items-center h-10 rounded px-3 text-sm text-logo font-medium hover:bg-gray-100 transition-colors";
+    "flex items-center h-10 rounded px-3 text-xl text-logo font-bold hover:bg-gray-100 transition-colors dark:text-white dark:hover:!bg-gray-800";
   navDiv.appendChild(navA);
 });
 const nav = document.getElementById("nav");
@@ -67,3 +67,7 @@ nav.appendChild(navDiv);
 
 const navTwo = document.getElementById("navTwo");
 navTwo.appendChild(navDivTwo);
+
+function darkMode() {
+  document.documentElement.classList.toggle("dark");
+}
